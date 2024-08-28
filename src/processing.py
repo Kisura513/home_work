5b979b1823db9944a4a1f1b37a63a2eaad2a65a5
German <Gerasuper2017@gmail.com>
2024-08-28 18:57:44 +0500

    ready

--- src/processing.py
+++ src/processing.py
@@ -1,7 +1,8 @@
 from typing import List, Any
 
 
-def filter_by_state(list_id: list, state="EXECUTED") -> list[Any]:
+def filter_by_state(list_id: list, state="CANCELED") -> list[Any]:
+    """ Функция фильтрации по ключу state """
     state_new_list = []
     for info in list_id:
         for value in info.values():
@@ -11,6 +12,7 @@ def filter_by_state(list_id: list, state="EXECUTED") -> list[Any]:
 
 
 def sort_by_date(list_id: list) -> list[Any]:
+    """ Сортировка списка словарей по дате """
     sorted_list = sorted(list_id, key=lambda x: x['date'], reverse=True)
     return sorted_list
 
@@ -20,3 +22,9 @@ print(sort_by_date([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18
                     {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
                     {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
                    ))
+
+print(filter_by_state([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+                       {'id': 939719570, 'state': 'CANCELED', 'date': '2018-06-30T02:08:58.425572'},
+                       {'id': 594226727, 'state': 'EXECUTED', 'date': '2018-09-12T21:27:25.241689'},
+                       {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
+                      ))
